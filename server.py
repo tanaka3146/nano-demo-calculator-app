f14895864f53f4ecf554b4e2e5ef087da586d727
roshan pious <80870643+tanaka3146@users.noreply.github.com>
2023-07-25 07:42:17 +0530

    test

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return jsonify("hello world")
+    return jsonify("hello world!")
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
